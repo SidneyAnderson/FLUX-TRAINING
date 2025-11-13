b755f66e0815b8eb958bd16e8df9e1f7a0a4c3c8
SidneyAnderson <sidney.s.anderson@gmail.com>
2025-11-12 21:02:29 -0700

    new kernels

--- kernels/setup.py
+++ kernels/setup.py
@@ -3,10 +3,12 @@ Setup script for Blackwell (RTX 5090) custom CUDA kernels
 Compiles with CUDA 13.0 and native sm_120 support
 """
 
+import os
+import shutil
+
+import torch
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-import torch
-import os
 
 # Ensure CUDA 13.0 is used
 cuda_home = os.environ.get('CUDA_HOME', '/usr/local/cuda')
@@ -20,6 +22,49 @@ print(f"PyTorch version: {torch.__version__}")
 print(f"PyTorch CUDA version: {torch.version.cuda}")
 print("=" * 60)
 
+is_windows = os.name == "nt"
+
+torch_include_paths = torch.utils.cpp_extension.include_paths()
+
+extra_cxx_args = [
+    '/O2',
+    '/std:c++17',
+] if is_windows else [
+    '-O3',
+    '-std=c++17',
+    '-fPIC',
+]
+
+common_nvcc_args = [
+    '-O3',
+    '-gencode=arch=compute_120,code=sm_120',
+    '-gencode=arch=compute_120,code=compute_120',
+    '--use_fast_math',
+    '-std=c++17',
+    '--expt-relaxed-constexpr',
+    '--expt-extended-lambda',
+    '--ptxas-options=-v',
+    '--ptxas-options=-warn-spills',
+    '--ptxas-options=-warn-lmem-usage',
+    '-lineinfo',
+    '--threads', '4',
+    '-DCUDA_VERSION=13000',
+    '-DBLACKWELL_ARCH=1',
+]
+
+if is_windows:
+    common_nvcc_args.extend([
+        '-Xcompiler', '/MD',
+    ])
+else:
+    common_nvcc_args.extend([
+        '-Xcompiler', '-fPIC',
+        '-Xcompiler', '-Wall',
+    ])
+
+library_dir = os.path.join(cuda_home, 'lib', 'x64') if is_windows else os.path.join(cuda_home, 'lib64')
+ninja_available = shutil.which('ninja') is not None
+
 setup(
     name='blackwell_flux_kernels',
     version='1.0.0',
@@ -29,50 +74,9 @@ setup(
         CUDAExtension(
             name='blackwell_flux_kernels',
             sources=['blackwell_kernels.cu'],
-            extra_compile_args={
-                'cxx': [
-                    '-O3',
-                    '-std=c++17',
-                    '-fPIC',
-                ],
-                'nvcc': [
-                    '-O3',
-                    # Native sm_120 for RTX 5090
-                    '-gencode=arch=compute_120,code=sm_120',
-                    # Also include compute_120 for JIT
-                    '-gencode=arch=compute_120,code=compute_120',
-                    # Fast math optimizations
-                    '--use_fast_math',
-                    # C++17 support
-                    '-std=c++17',
-                    # Extended lambdas and constexpr
-                    '--expt-relaxed-constexpr',
-                    '--expt-extended-lambda',
-                    # Verbose output
-                    '--ptxas-options=-v',
-                    # Warnings
-                    '--ptxas-options=-warn-spills',
-                    '--ptxas-options=-warn-lmem-usage',
-                    # Debug info for profiling
-                    '-lineinfo',
-                    # Parallel compilation
-                    '--threads', '4',
-                    # Additional optimizations
-                    '-Xcompiler', '-fPIC',
-                    '-Xcompiler', '-Wall',
-                    # Define CUDA version
-                    '-DCUDA_VERSION=13000',
-                    # Enable Blackwell features
-                    '-DBLACKWELL_ARCH=1',
-                ]
-            },
-            include_dirs=[
-                torch.utils.cpp_extension.include_paths()[0],
-                os.path.join(cuda_home, 'include'),
-            ],
-            library_dirs=[
-                os.path.join(cuda_home, 'lib64'),
-            ],
+            extra_compile_args={'cxx': extra_cxx_args, 'nvcc': common_nvcc_args},
+            include_dirs=torch_include_paths + [os.path.join(cuda_home, 'include')],
+            library_dirs=[library_dir],
             libraries=[
                 'cudart',
                 'cublas',
@@ -83,8 +87,8 @@ setup(
     ],
     cmdclass={
         'build_ext': BuildExtension.with_options(
-            use_ninja=True,
-            use_cuda=True
+            use_ninja=ninja_available,
+            use_cuda=True,
         )
     },
     python_requires='>=3.11',
